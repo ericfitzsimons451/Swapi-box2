eacf16730984a04f01fcf07f0b38ee6e1661681e
Eric Fitzsimons <ericfitzsimons451@gmail.com>
2019-03-17 17:21:13 -0600

    adds try/catch to fetchAnything

--- src/Components/FetchAnything/FetchAnything.js
+++ src/Components/FetchAnything/FetchAnything.js
@@ -1,7 +1,11 @@
 export const fetchAnything = async (url) => {
-        const response = await fetch(url)
-        const data = response.json()
-        return data
+        try {
+                const response = await fetch(url)
+                const data = response.json()
+                return data
+        } catch (error) {
+                throw new Error('Error fetching data.')
+        }
 }
 
 export default fetchAnything
